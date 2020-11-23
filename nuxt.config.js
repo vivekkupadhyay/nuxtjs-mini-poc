a7cd07416a079f8dd38f8647d8460e11698c54f1
vivekupadhyay <vivek.upadhyay@schoolapply.com>
2020-11-23 12:29:48 +0530

    added fix for font path build issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     // SCSS file in the project
-    '~/assets/stylesheets/scss/app.scss'
+    '~/assets/stylesheets/app.scss'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
